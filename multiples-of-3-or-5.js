d3699f7d462ed7548a0740ae64248d8b7d0d79ac
“JustinVx” <justin.venbrux@gmail.com>
2016-10-16 18:34:57 +0200

    From sum_of_multiples_of_3 to sum_of_multiples_of_3_or_5.

--- multiples-of-3-or-5.js
+++ multiples-of-3-or-5.js
@@ -1,13 +1,12 @@
 
-function sum_of_multiples_of_3(should_be_below) {
-  var current_sum_of_multiples_of_3 = 0;
+function sum_of_multiples_of_3_or_5(should_be_below) {
+  var current_sum_of_multiples_of_3_or_5 = 0;
   for (i = 1; i < should_be_below; i++) {
-  	if (i % 3 === 0) {
-      current_sum_of_multiples_of_3 += i;
+  	if (i % 3 === 0 || i % 5 === 0) {
+      current_sum_of_multiples_of_3_or_5 += i;
   	}
   }
-  return current_sum_of_multiples_of_3;
+  return current_sum_of_multiples_of_3_or_5;
 }
 
-
-console.log(sum_of_multiples_of_3(10));
+console.log(sum_of_multiples_of_3_or_5(10));
