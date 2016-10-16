c9a4e11cd56c9604a5fc38bf7db9ffb7ad405afd
“JustinVx” <justin.venbrux@gmail.com>
2016-10-16 18:57:11 +0200

    Made function to print all fibonacci numbers below a certain number.

--- even-fibonacci-numbers.js
+++ even-fibonacci-numbers.js
@@ -1,1 +1,15 @@
 console.log("Problem 2!")
+
+function print_fibonacci_numbers_below(does_not_exceed) {
+  var i1 = 1;
+  console.log(i1);
+  var i2 = 2;
+  while(i2 < does_not_exceed){
+    console.log(i2);
+    new_i = i1 + i2;
+    i1 = i2;
+    i2 = new_i;
+  }
+}
+
+print_fibonacci_numbers_below(100);
