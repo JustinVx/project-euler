0089d32be5cbe75c032b51407ee081c4244d989a
“JustinVx” <justin.venbrux@gmail.com>
2016-10-16 19:03:33 +0200

    Created a function to solve problem 2 and solved problem 2 using JavaScript.

--- even-fibonacci-numbers.js
+++ even-fibonacci-numbers.js
@@ -1,15 +1,18 @@
-console.log("Problem 2!")
+console.log("Answer to problem 2:")
 
-function print_fibonacci_numbers_below(does_not_exceed) {
+function sum_of_even_fibonacci_numbers_below(does_not_exceed) {
+  var current_sum_of_even_fibonacci_numbers = 0;
   var i1 = 1;
-  console.log(i1);
   var i2 = 2;
   while(i2 < does_not_exceed){
-    console.log(i2);
+    if (i2 % 2 == 0) {
+      current_sum_of_even_fibonacci_numbers += i2;
+    }
     new_i = i1 + i2;
     i1 = i2;
     i2 = new_i;
   }
+  console.log("The sum of the even fibonacci numbers below " + does_not_exceed + " is " + current_sum_of_even_fibonacci_numbers + "!");
 }
 
-print_fibonacci_numbers_below(100);
+sum_of_even_fibonacci_numbers_below(4000000000);
